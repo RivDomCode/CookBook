169c4ad6943ee3e8f29a77abf249b87ad0cd0e29
Pedro RivDom <66123144+RivDomCode@users.noreply.github.com>
2022-02-03 20:15:37 +0100

    started header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import cookLogo from "../img/cooklogo.png"; //Logo taken from this place: <a href="https://www.flaticon.com/free-icons/food-and-restaurant" title="food and restaurant icons">Food and restaurant icons created by Icongeek26 - Flaticon</a>
+import brokenChili from "../img/brokenChili.png";
+import chiles from "../img/chiles.png";
+import food from "../img/food.png";
 
 export const Navbar = () => {
   return (
-    <nav className="navbar">
-      <div className="navbar__logo">
-        <img src={cookLogo} alt="logo" className="navbar__logo__img" />
-        <span className="navbar__logo__titles">The CookBook</span>
+    <header>
+      <nav className="navbar">
+        <div className="navbar__logo">
+          <img src={cookLogo} alt="logo" className="navbar__logo__img" />
+          <span className="navbar__logo__titles">The CookBook</span>
+        </div>
+        <div className="navbar__links">
+          <Link to="/">
+            <span className="navbar__links__link">My Recipes</span>
+          </Link>
+          <Link to="APISearch">
+            <span className="navbar__links__link">API Search</span>
+          </Link>
+        </div>
+      </nav>
+      <div className="header-img">
+        <img src={food} alt="" className="delicious-img" />
+        <img src={chiles} alt="" className="chiles" />
       </div>
-      <div className="navbar__links">
-        <Link to="/">
-          <span className="navbar__links__link">My Recipes</span>
-        </Link>
-        <Link to="APISearch">
-          <span className="navbar__links__link">API Search</span>
-        </Link>
-      </div>
-    </nav>
+    </header>
   );
 };
