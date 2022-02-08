0e83b8f6508e6b8ac18e2b19268ee85969f2aad8
Pedro RivDom <66123144+RivDomCode@users.noreply.github.com>
2022-02-08 19:41:19 +0100

    new recipe form style, and add img  useref

--- src/components/NewRecipeScreen.jsx
+++ src/components/NewRecipeScreen.jsx
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useRef } from "react";
 
 export const NewRecipeScreen = () => {
+  const addImg = useRef();
+
+  const handleAddImg = (e) => {
+    e.preventDefault();
+    addImg.current.click();
+  };
+
   return (
     <div className="new-recipe">
-      <p>New Recipe</p>
       <form className="new-recipe__form">
-        <input type="text" placeholder="Recipe Title" />
-        <input type="text" placeholder="Recipe Title" />
-        <input type="text" placeholder="Recipe Title" />
+        <div className="new-recipe__form__inputs">
+          <h2>New Recipe</h2>
+          <input type="text" placeholder="Recipe Title" />
+          <input type="text" placeholder="Ingredients" />
+          <textarea type="text" placeholder="Elaboration" />
+          <input type="file" className="img-invisible" ref={addImg} />{" "}
+          <div className="new-recipe__form__btns">
+            <button className="addImg-btn" onClick={handleAddImg}>
+              <i class="far fa-image"></i>Add an Image
+            </button>
+            <button className="saveRecipe-btn">
+              <i class="fas fa-cloud-download-alt"></i>Save Recipe
+            </button>
+          </div>
+        </div>
       </form>
     </div>
   );
