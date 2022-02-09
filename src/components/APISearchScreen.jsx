32de94ec8ff7ffee8ece2302af726d13727137ca
Pedro RivDom <66123144+RivDomCode@users.noreply.github.com>
2022-02-09 19:29:02 +0100

    not too much

--- src/components/APISearchScreen.jsx
+++ src/components/APISearchScreen.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import { CardRecipe } from "./CardRecipe";
 
 export const APISearchScreen = () => {
-  return <div>API search</div>;
+  return (
+    <div className="APIsearch">
+      <form>
+        <h3>GET INSPIRATION FROM SPOONACULAR API</h3>
+        <input type="text" placeholder="Type an ingredient here..." />
+        <button className="searchAPI-btn">Search</button>
+      </form>
+      <div className="my-recipes__recipesLayout">
+        <CardRecipe />
+        <CardRecipe />
+        <CardRecipe />
+      </div>
+    </div>
+  );
 };
--- src/components/NewRecipeScreen.jsx
+++ src/components/NewRecipeScreen.jsx
@@ -11,12 +11,19 @@ export const NewRecipeScreen = () => {
   return (
     <div className="new-recipe">
       <form className="new-recipe__form">
+        <button className="close-btn">x</button>
         <div className="new-recipe__form__inputs">
           <h2>New Recipe</h2>
           <input type="text" placeholder="Recipe Title" />
           <input type="text" placeholder="Ingredients" />
           <textarea type="text" placeholder="Elaboration" />
           <input type="file" className="img-invisible" ref={addImg} />{" "}
+          <div className="new-recipe__form__img-container">
+            <img
+              src="https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032_960_720.jpg"
+              alt=""
+            />
+          </div>
           <div className="new-recipe__form__btns">
             <button className="addImg-btn" onClick={handleAddImg}>
               <i class="far fa-image"></i>Add an Image
