2a814c516ab3dc3950a67b7966ed3e32a3b470bf
Pedro RivDom <66123144+RivDomCode@users.noreply.github.com>
2022-04-08 13:28:02 +0200

    loading component done

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,9 +1,9 @@
-import { DualRing } from "react-spinners-css";
+// import { DualRing } from "react-spinners-css";
 
-export const Loading = () => {
-  return (
-    <div className="loading">
-      <DualRing color="#5c9bdb" size={100} />
-    </div>
-  );
-};
+// export const Loading = () => {
+//   return (
+//     <div className="loading">
+//       <DualRing color="#5c9bdb" size={100} />
+//     </div>
+//   );
+// };
